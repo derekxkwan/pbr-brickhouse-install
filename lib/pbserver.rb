2395663d8b866dc5f0c09f9aea680822432ae9e4
Derek Kwan <derek.x.kwan@gmail.com>
2018-06-15 04:50:12 -0700

    got post working

--- lib/pbserver.rb
+++ lib/pbserver.rb
@@ -3,16 +3,25 @@ require 'sinatra/base'
 require 'thin'
 
 class PbServer < Sinatra::Base
+  attr_accessor :msgs
+  
   set :public_folder, File.expand_path('../../static', __FILE__)
+  set :views, File.expand_path('../../views', __FILE__)
   set :port, 9000
   set :server, :thin
 
+  def initialize
+    super()
+    @msgs = []
+    end
+  
   get "/" do
     File.read(File.join("static", "index.html"))
     end
 
-  post "/" do
-    p params
+  post "/submit" do
+    @msgs.unshift(params[:message])
+    erb :submitted
     end
 
   run!
